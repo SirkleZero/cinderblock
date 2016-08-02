f98d32b3c40d39fbec9a32b02ca9d46855df207b
Joshua Gall <josh@imtraum.com>
2016-08-01 23:16:35 -0500

    Minor Refactoring and Documentation Updates

--- Cinderblock/Security/SecuredPassword.cs
+++ Cinderblock/Security/SecuredPassword.cs
@@ -4,66 +4,100 @@ using System.Security.Cryptography;
 namespace Cinderblock.Security
 {
     /// <summary>
-    /// Provides an object container for a password that has been hashed with a specified nonce value.
+    /// Provides an object container for a password that has been hashed with a specified salt value.
     /// </summary>
     public sealed class SecuredPassword
     {
         /// <summary>
+        /// Specifies the default minimum size in bytes for the salt.
+        /// </summary>
+        private const int defaultMinimumSaltSize = 48;
+
+        /// <summary>
+        /// Specifies the default maximum size in bytes for the salt.
+        /// </summary>
+        private const int defaultMaximumSaltSize = 64;
+
+        /// <summary>
         /// Creates an instance of a <see cref="SecuredPassword"/> object that represents a password that has been secured by a Hash.
         /// </summary>
         /// <param name="hashedPassword">The hashed password.</param>
-        /// <param name="nonce">The nonce that was used to hash the password.</param>
-        private SecuredPassword(string hashedPassword, byte[] nonce)
+        /// <param name="salt">The salt that was used to hash the password.</param>
+        private SecuredPassword(string hashedPassword, byte[] salt)
         {
-            if (string.IsNullOrEmpty(hashedPassword))
-            {
-                throw new ArgumentNullException("hashedPassword");
-            }
-            if(nonce == null || nonce.Length.Equals(0))
-            {
-                throw new ArgumentOutOfRangeException("nonce", "The nonce value cannot be null or have a length of zero bytes.");
-            }
-
             this.HashedPassword = hashedPassword;
-            this.Nonce = nonce;
+            this.Salt = salt;
         }
 
         /// <summary>
         /// The hashed password.
         /// </summary>
-        public string HashedPassword { get; set; }
+        public string HashedPassword { get; private set; }
 
         /// <summary>
-        /// The nonce that was used to hash the password.
+        /// The salt that was used to hash the password.
         /// </summary>
-        public byte[] Nonce { get; set; }
+        public byte[] Salt { get; private set; }
 
         /// <summary>
-        /// Creates a new instance of the <see cref="SecuredPassword"/> object by hashing a clear text password with a cryptographically 
-        /// random nonce size between 48 and 64 bytes in length.
+        /// Creates an instance of the <see cref="SecuredPassword"/> object by hashing a clear text password with a new cryptographically 
+        /// random salt size between 48 and 64 bytes in length.
         /// </summary>
         /// <param name="cleartextPassword">The password to hash.</param>
-        /// <returns>A <see cref="SecuredPassword"/> containing the hashed password and it's corresponding nonce.</returns>
+        /// <returns>A <see cref="SecuredPassword"/> containing the hashed password and it's corresponding salt.</returns>
         public static SecuredPassword Create(string cleartextPassword)
         {
-            return SecuredPassword.Create(cleartextPassword, 48, 64);
+            return SecuredPassword.Create(cleartextPassword, SecuredPassword.defaultMinimumSaltSize, SecuredPassword.defaultMaximumSaltSize);
         }
 
         /// <summary>
-        /// Creates a new instance of the <see cref="SecuredPassword"/> object by hashing a clear text password with a cryptographically 
-        /// random nonce size between 48 and 64 bytes in length.
+        /// Creates an instance of the <see cref="SecuredPassword"/> object by hashing a clear text password with a new cryptographically 
+        /// random salt size between the minimum and maximum byte sizes.
         /// </summary>
         /// <param name="cleartextPassword">The password to hash.</param>
-        /// <param name="minimumSaltBytes">The minimum length in bytes for the nonce value.</param>
-        /// <param name="maximumSaltBytes">The maximum length in bytes for the nonce value.</param>
-        /// <returns>A <see cref="SecuredPassword"/> containing the hashed password and it's corresponding nonce.</returns>
+        /// <param name="minimumSaltBytes">The minimum length in bytes for the salt value.</param>
+        /// <param name="maximumSaltBytes">The maximum length in bytes for the salt value.</param>
+        /// <returns>A <see cref="SecuredPassword"/> containing the hashed password and it's corresponding salt.</returns>
         public static SecuredPassword Create(string cleartextPassword, int minimumSaltBytes, int maximumSaltBytes)
         {
+            if (string.IsNullOrEmpty(cleartextPassword))
+            {
+                throw new ArgumentNullException("cleartextPassword");
+            }
+            if (minimumSaltBytes.Equals(0) || minimumSaltBytes > maximumSaltBytes)
+            {
+                throw new ArgumentOutOfRangeException("minimumSaltSize", "The minimum salt size cannot be zero, or be greater than the maximum size.");
+            }
+            if (maximumSaltBytes.Equals(0))
+            {
+                throw new ArgumentOutOfRangeException("maximumSaltSize", "The maximum salt size cannot be zero.");
+            }
+
             using (var provider = new RNGCryptoServiceProvider())
             {
                 var salt = provider.GenerateSalt(minimumSaltBytes, maximumSaltBytes);
-                return new SecuredPassword(cleartextPassword.Hash(HashType.SHA512, salt), salt);
+                return SecuredPassword.Create(cleartextPassword, salt);
             }
+        }
+
+        /// <summary>
+        /// Creates an instance of the <see cref="SecuredPassword"/> object by hashing a clear text password with a predefined salt.
+        /// </summary>
+        /// <param name="cleartextPassword">The password to hash.</param>
+        /// <param name="salt">The salt value to use when hashing the password.</param>
+        /// <returns>A <see cref="SecuredPassword"/> containing the hashed password and it's corresponding salt.</returns>
+        public static SecuredPassword Create(string cleartextPassword, byte[] salt)
+        {
+            if (string.IsNullOrEmpty(cleartextPassword))
+            {
+                throw new ArgumentNullException("cleartextPassword");
+            }
+            if (salt == null || salt.Length.Equals(0))
+            {
+                throw new ArgumentOutOfRangeException("salt", "The salt value cannot be null or have an array length equal to zero.");
+            }
+
+            return new SecuredPassword(cleartextPassword.Hash(HashType.SHA512, salt), salt);
         }
     }
 }
